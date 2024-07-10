90c342e5e6b99117387b14a33d840561d006f4fd
Coding of Geograpikals chanel <161205801+PappyGio@users.noreply.github.com>
2024-07-10 13:23:45 +0800

    Create pair o mancer.c

---
+++ pair o mancer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//define linked lIST
+
+typedef struct node{
+	int data;
+	struct Node *next;
+}Node;
+
+//create new node 
+
+Node* createNode(int data){
+	
+}
